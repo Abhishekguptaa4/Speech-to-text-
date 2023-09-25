78504c5493442489f8edb4f086a3a856db43cd90
Abhishek Gupta <122620143+Abhishekguptaa4@users.noreply.github.com>
2023-09-26 00:43:02 +0530

    Add files via upload

---
+++ language.js
@@ -0,0 +1,39 @@
+const languages = [
+  {
+    no: "0",
+    name: "Auto",
+    native: "Detect",
+    code: "auto",
+  },
+  
+  {
+    no: "1",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },
+  {
+    no: "2",
+    name: "Urdu",
+    native: "اردو",
+    code: "ur",
+  },
+  {
+    no: "3",
+    name: "Bengali",
+    native: "Հայերէն",
+    code: "bn",
+  },
+  {
+    no: "4",
+    name: "Punjabi",
+    native: "ਪੰਜਾਬੀ",
+    code: "pn",
+  },
+  {
+    no: "5",
+    name: "Indian English",
+    native: "Indian English",
+    code: "en",
+  }
+];
